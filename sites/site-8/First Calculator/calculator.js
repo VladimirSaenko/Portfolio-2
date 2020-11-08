7f66d7de7a769d45cbe7590d905d74777ec7c707
Vladimir Saenko <vova.saenko1@gmail.com>
2020-11-08 13:48:25 +0200

    add First Calculator

---
+++ sites/site-8/First Calculator/calculator.js
@@ -0,0 +1,8 @@
+
+"use strict";
+
+var musicbutt = document.getElementById('musicbutt-3');
+musicbutt.addEventListener('click',function () {
+    document.getElementById('sound').play();            
+})
+
