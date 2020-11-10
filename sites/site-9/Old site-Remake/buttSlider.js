7f5629edac555dd86b03182c95cfae6f26e6a6f8
Vladimir Saenko <vova.saenko1@gmail.com>
2020-11-10 15:34:38 +0200

    add site # 9
    add site "Old site | Remake new version"

---
+++ sites/site-9/Old site-Remake/buttSlider.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+function lifestyleText() {
+    document.getElementById('tittle').innerHTML = "Lifestyle";
+    document.getElementById('image').src = "img/two_girls_image.png";
+}
+
+function photodiaryText() {
+    document.getElementById('tittle').innerHTML = "Photodiary";
+    document.getElementById('image').src = "img/coffe_image.png";
+}
+
+function musicText() {
+    document.getElementById('tittle').innerHTML = "Music";
+    document.getElementById('image').src = "img/girl_image.png";
+}
+
+function travelText() {
+    document.getElementById('tittle').innerHTML = "Travel";
+    document.getElementById('image').src = "img/image_dorogi_v_Russia.png";
+}
