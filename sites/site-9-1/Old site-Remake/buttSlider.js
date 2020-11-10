cd6f031f365f5f889c50a0ffe6c4e9fe47b4de71
Vladimir Saenko <vova.saenko1@gmail.com>
2020-11-10 16:33:15 +0200

    add new js script site 9-1

--- sites/site-9-1/Old site-Remake/buttSlider.js
+++ sites/site-9-1/Old site-Remake/buttSlider.js
@@ -4,19 +4,27 @@
 function lifestyleText() {
     document.getElementById('tittle').innerHTML = "Lifestyle";
     document.getElementById('image').src = "img/two_girls_image.png";
+    document.getElementById('lorem').style.visibility = "visible";
+    document.getElementById('calltittle').style.visibility = "hidden";
 }
 
 function photodiaryText() {
     document.getElementById('tittle').innerHTML = "Photodiary";
     document.getElementById('image').src = "img/coffe_image.png";
+    document.getElementById('lorem').style.visibility = "visible";
+    document.getElementById('calltittle').style.visibility = "hidden";
 }
 
 function musicText() {
     document.getElementById('tittle').innerHTML = "Music";
     document.getElementById('image').src = "img/girl_image.png";
+    document.getElementById('lorem').style.visibility = "visible";
+    document.getElementById('calltittle').style.visibility = "hidden";
 }
 
 function travelText() {
     document.getElementById('tittle').innerHTML = "Travel";
     document.getElementById('image').src = "img/image_dorogi_v_Russia.png";
+    document.getElementById('lorem').style.visibility = "visible";
+    document.getElementById('calltittle').style.visibility = "hidden";
 }
