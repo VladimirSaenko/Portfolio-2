56afc43bc6a528481f6fab84c89ee0ebc2164d74
Vladimir Saenko <vova.saenko1@gmail.com>
2020-12-11 00:17:09 +0200

    update parrallaxEffectMoonLight.js in site 9-2
    update parrallaxEffectMoonLight.js in site "Parrallax"

--- sites/site-9-2/Parrallax/sites/Parallax-Moon-Light/parallaxEffectMoonLight.js
+++ sites/site-9-2/Parrallax/sites/Parallax-Moon-Light/parallaxEffectMoonLight.js
@@ -6,9 +6,26 @@ let moon = document.getElementById("moon");
 let mountain = document.getElementById("mountain");
 let road = document.getElementById("road");
 let text = document.getElementById("text");
+let tittleFooter = document.getElementById("footer-tittle");
+let gitimgLink = document.getElementById("gitImg");
+let isTFvis = false;
+
+window.addEventListener('scroll', function() {
+    tittleFooter.style.transition = '3.5s';
+    tittleFooter.style.transform = 'translateX(-0%)';
+    isTFvis = true;
+    function visGitImg() {
+        gitimgLink.style.transition = '3.5';
+        gitimgLink.style.visibility = 'visible';
+        gitimgLink.style.transform = 'translateX(240%)';
+    } 
+    if(isTFvis == true) {
+        setTimeout(visGitImg,3500);
+    }
+})
 
 window.addEventListener('scroll',function() {
-    var value = window.scrollY;
+    let value = window.scrollY;
     bg.style.top = value * 0.5 + 'px';
     moon.style.left = value * 0.5 + 'px';
     mountain.style.top = value * 0.15 + 'px';
