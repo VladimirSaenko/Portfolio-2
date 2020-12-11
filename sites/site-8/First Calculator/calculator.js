3ad34327ed3db05e3507b40e0c45c8e43964774e
Vladimir Saenko <vova.saenko1@gmail.com>
2020-12-11 13:58:49 +0200

    add two new buttons and audio in site 8
    0.25 and 0.5

--- sites/site-8/First Calculator/calculator.js
+++ sites/site-8/First Calculator/calculator.js
@@ -1,12 +1,19 @@
 
 "use strict";
 
-var musicbutt = document.getElementById('musicbutt-3');
+let musicbutt = document.getElementById('musicbutt-3');
 musicbutt.addEventListener('click',function () {
-    document.getElementById('sound').play();            
+    document.getElementById('sound').play();
 })
-
-var musicbutt40 = document.getElementById('musicbutt40');
-    musicbutt40.addEventListener('click',function () {
+let musicbutt40 = document.getElementById('musicbutt40');
+musicbutt40.addEventListener('click',function () {
     document.getElementById('sound40').play();
+})
+let musicButt0_25 = document.getElementById('alc-1');
+let musicButt0_5 = document.getElementById('alc-2');
+musicButt0_25.addEventListener('click', function() {
+    document.getElementById('alcosound').play();
+})
+musicButt0_5.addEventListener('click', function() {
+    document.getElementById('alcosound').play();
 })
