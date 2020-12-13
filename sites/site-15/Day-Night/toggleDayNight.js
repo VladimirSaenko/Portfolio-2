694ed0b69a8b4e807dd9499056645307cf91a471
Vladimir Saenko <vova.saenko1@gmail.com>
2020-12-13 14:41:14 +0200

    add site-15
    add site "Day-Night example"

---
+++ sites/site-15/Day-Night/toggleDayNight.js
@@ -0,0 +1,30 @@
+"use strict";
+
+function toggleScene() {
+    let change = document.querySelector('.scene');
+    change.classList.toggle('active');
+    starsSpawn();
+}
+
+function starsSpawn() {
+    let count = 300;
+    let i = 0;
+    while(i < count) {
+        // console.log(i);
+        let scene = document.querySelector('.scene');
+        let star = document.createElement('i');
+        let x = Math.floor(Math.random() * window.innerWidth);
+        let y = Math.floor(Math.random() * window.innerHeight);
+        let size = Math.random() * 2;
+        let duration = Math.random() * 10;
+        star.style.left = x + 'px';
+        star.style.top = y + 'px';
+        star.style.width = size + 'px';
+        star.style.height = size + 'px';
+        star.style.animationDuration = 5 + duration + 's';
+        star.style.animationDelay = duration + 's';
+        scene.appendChild(star);
+        i++;
+    }
+}
+starsSpawn();
