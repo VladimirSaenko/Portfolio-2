4ce02289e061023b66402abb3d62b37b111a356f
Vladimir Saenko <vova.saenko1@gmail.com>
2020-12-16 23:50:47 +0200

    update calculator.js script in site-8
    update calculator.js script in site "First Calculator"

--- sites/site-8/First Calculator/calculator.js
+++ sites/site-8/First Calculator/calculator.js
@@ -10,10 +10,11 @@ musicbutt40.addEventListener('click',function () {
     document.getElementById('sound40').play();
 })
 let musicButt0_25 = document.getElementById('alc-1');
-let musicButt0_5 = document.getElementById('alc-2');
 musicButt0_25.addEventListener('click', function() {
     document.getElementById('alcosound').play();
 })
+let musicButt0_5 = document.getElementById('alc-2');
+
 musicButt0_5.addEventListener('click', function() {
     document.getElementById('alcosound').play();
 })
