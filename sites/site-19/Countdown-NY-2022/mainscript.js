ed0e249d194dc07d791b751e248f9655aa621d32
Vladimir Saenko <vova.saenko1@gmail.com>
2021-01-01 13:58:35 +0200

    add two album

--- sites/site-19/Countdown-NY-2022/mainscript.js
+++ sites/site-19/Countdown-NY-2022/mainscript.js
@@ -1,7 +1,7 @@
 
 "use strict"
 
-const countDate = new Date('jan 1, 2021 00:00:00').getTime();
+const countDate = new Date('jan 1, 2022 00:00:00').getTime();
 
 function newYear() {
     const nowDate = new Date().getTime();
