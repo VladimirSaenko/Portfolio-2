6f3d892ede75601e2e03fdc4cd72bde6d425e7f4
Vladimir Saenko <vova.saenko1@gmail.com>
2021-02-07 14:10:39 +0200

    add js script in site-28
    add js script in site-"GloveOfThanos-site"

---
+++ sites/site-28/GloveOfThanos/disappearanceScript.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+
+const banner = document.getElementsByClassName('banner')[0];
+const blocks = document.getElementsByClassName('blocks');
+
+for(let i = 1; i < 400; i++) {
+    banner.innerHTML += "<div class='blocks'></div>";
+    const duration = Math.random() * 5;
+    blocks[i].style.animationDuration = 2 + duration + 's';
+    blocks[i].style.animationDelay = duration + 's';
+}
+
+const section = document.querySelector('section');
+setTimeout(function() {
+    section.classList.add('active');
+}, 13500);
