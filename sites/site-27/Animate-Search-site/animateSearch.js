5e74f67556c392d309674a625dafc5bbcea0dd15
Vladimir Saenko <vova.saenko1@gmail.com>
2021-02-07 11:41:24 +0200

    update in site-27
    update in site-"Animate-Search-example"

--- sites/site-27/Animate-Search-site/animateSearch.js
+++ sites/site-27/Animate-Search-site/animateSearch.js
@@ -1,17 +1,24 @@
 
-"use strict";
-
-
 const searchBtn = document.querySelector(".search-btn");
 const searchBox = document.querySelector(".search-box");
 const cancelBtn = document.querySelector(".cancel-btn");
 const searchInput = document.querySelector("input");
+const searchData = document.querySelector(".search-data");
 
 searchBtn.onclick = () => {
     searchBox.classList.add("active");
     searchInput.classList.add("active");
     searchBtn.classList.add("active");
     cancelBtn.classList.add("active");
+
+    if(searchInput.value != "") {
+        let values = searchInput.value;
+        searchData.classList.remove("active");
+        searchData.innerHTML = "Вы набрали " + values;
+        }
+        else {
+            searchData.innerHTML = "";
+        }
 }
 
 cancelBtn.onclick = () => {
@@ -19,5 +26,6 @@ cancelBtn.onclick = () => {
     searchInput.classList.remove("active");
     searchBtn.classList.remove("active");
     cancelBtn.classList.remove("active");
+    searchData.classList.add("active");
+    searchInput.value = "";
 }
-
