d36263bdf97f08c3a7693d108fd06fc7d9ce99cd
Vladimir Saenko <vova.saenko1@gmail.com>
2021-02-14 14:30:33 +0200

    add site-31
    add site-"Menu-animate-site-example"

---
+++ sites/site-31/Menu-animate-example/script.js
@@ -0,0 +1,21 @@
+
+"use strict";
+
+function toggleMenu() {
+    const menuToggle = document.querySelector(".menuToggle");
+    // menuToggle.classList.add('active');
+    menuToggle.classList.toggle('active');
+    const navigation = document.querySelector('.navigation');
+    // navigation.classList.add('active');
+    navigation.classList.toggle('active');
+}
+
+let menuItems = document.getElementsByClassName('menu-item');
+for(let i = 0; i < menuItems.length; i++) {
+    menuItems[i].onmouseout = () => {
+        document.getElementById('slide').src = 'images/black.jpg';
+    }
+    menuItems[i].onmouseenter = () => {
+        document.getElementById('slide').src = `images/img${i+1}.jpg`;
+    }
+}
