2371387e6956df242e7a4c75e44cd752f3da9bbf
Vladimir Saenko <vova.saenko1@gmail.com>
2021-03-10 17:20:12 +0200

    update medias in site-9-1
    update medias in site-9-1

--- sites/site-9-1/Old-site-Remake/buttSlider.js
+++ sites/site-9-1/Old-site-Remake/buttSlider.js
@@ -7,7 +7,8 @@ function lifestyleText() {
     document.getElementById('lorem').style.visibility = "visible";
     document.getElementById('calltittle').style.visibility = "hidden";
     if(window.matchMedia("max-width:600px")) {
-        document.getElementById('tittle').style.fontSize = "75px";
+        document.getElementById('tittle').style.fontSize = "45px";
+        document.getElementById('tittle').style.letterSpacing = "3px";
     }
 }
 
@@ -17,7 +18,8 @@ function photodiaryText() {
     document.getElementById('lorem').style.visibility = "visible";
     document.getElementById('calltittle').style.visibility = "hidden";
     if(window.matchMedia("max-width:600px")) {
-        document.getElementById('tittle').style.fontSize = "60px";
+        document.getElementById('tittle').style.fontSize = "40px";
+        document.getElementById('tittle').style.letterSpacing = "2px";
     }
 }
 
@@ -26,6 +28,10 @@ function musicText() {
     document.getElementById('image').src = "img/girl_image.png";
     document.getElementById('lorem').style.visibility = "visible";
     document.getElementById('calltittle').style.visibility = "hidden";
+    if(window.matchMedia("max-width:600px")) {
+        document.getElementById('tittle').style.fontSize = "55px";
+        document.getElementById('tittle').style.letterSpacing = "5px";
+    }
 }
 
 function travelText() {
@@ -33,4 +39,8 @@ function travelText() {
     document.getElementById('image').src = "img/image_dorogi_v_Russia.png";
     document.getElementById('lorem').style.visibility = "visible";
     document.getElementById('calltittle').style.visibility = "hidden";
+    if(window.matchMedia("max-width:600px")) {
+        document.getElementById('tittle').style.fontSize = "65px";
+        document.getElementById('tittle').style.letterSpacing = "5px";
+    }
 }
