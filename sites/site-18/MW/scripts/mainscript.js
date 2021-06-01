ea55df6d89bc77384a7874da3b12484e1faa6797
Vladimir Saenko <vova.saenko1@gmail.com>
2021-06-01 12:39:47 +0300

    del resultColor in site-18
    del resultColor in site-18

--- sites/site-18/MW/scripts/mainscript.js
+++ sites/site-18/MW/scripts/mainscript.js
@@ -4,7 +4,6 @@
 let userLim = document.getElementById('limit');
 let resultForm = document.getElementById('form');
 let resultText = document.getElementById('resultText');
-let resultColor = document.getElementById('resultColor');
 let color1 = document.getElementById('color1');
 let color2 = document.getElementById('color2');
 let colorcost1 = getRandomInt(0,100);
@@ -114,8 +113,7 @@ function mainFunc() {
 function calculateMinimalWay() {
     if(userLim.value > way1 && userLim.value > way2) {
         resultForm.style.visibility = 'visible';
-        resultText.textContent = "Ваш лимит позволяет вам воспользоваться обеями путями";
-        resultColor.style.visibility = "hidden";  
+        resultText.textContent = "Ваш лимит позволяет вам воспользоваться обеями путями";  
     }
    
     if(way1 < way2 && userLim.value >= way1 && userLim.value < way2) {
@@ -123,7 +121,6 @@ function calculateMinimalWay() {
         let wayColor1 = color1.style.background;
         resultText.textContent = "Первый путь за " + way1 + valuta + " с цветом- " + wayColor1 +
         " самый минимальный для вашего лимита";
-        resultColor.style.background = color1.style.background;
     }
    
     if(way2 < way1 && userLim.value >= way2 && userLim.value < way1) {
@@ -132,31 +129,26 @@ function calculateMinimalWay() {
         resultText.textContent = "Второй путь за " + way2 + valuta  + " с цветом- " + wayColor2 +  
         " самый минимальный для вашего лимита";
         resultcolorText.style.visibility = "visible";
-        resultColor.style.background = color2.style.background;
     }
    
     if(way1 == way2 && userLim.value > way1 && way2) {
         resultForm.style.visibility = 'visible';
         resultText.textContent = "Ваш лимит позволяет вам воспользоваться обеями путями";
-        resultColor.style.visibility = "hidden";
     }
    
     if(userLim.value < way1 && userLim.value < way2 || userLim.value == 0) {
         resultForm.style.visibility = 'visible';
         resultText.textContent = 'Ваш лимит очень маленький';
-        resultColor.style.visibility = 'hidden';
     }
    
     if(userLim.value < 0) {
         resultForm.style.visibility = 'visible';
         resultText.textContent = 'Ваш лимит минусовой, напишете число больше 0 ';
-        resultColor.style.visibility = 'hidden';
     }
  
     if(userLim.value == 0 && way1 == 0 && way2 == 0) {
         resultForm.style.visibility = 'visible';
         resultText.textContent = 'Введите лимит';
-        resultColor.style.visibility = 'hidden';
     }
 }
 
