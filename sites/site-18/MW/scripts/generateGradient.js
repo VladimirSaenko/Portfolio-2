1fc9805b8560179f2cbe2f30a800251f9b0728a7
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-07-29 15:52:14 +0300

    Upd script in site-18

--- sites/site-18/MW/scripts/generateGradient.js
+++ sites/site-18/MW/scripts/generateGradient.js
@@ -1,5 +1,5 @@
 
-"use strict"
+"use strict";
 
 let mainsection = document.getElementById("mainsection");
 mainsection.style.background = 'linear-gradient(' + getRandomInt(100,360) + 'deg' + ',' + 'rgb(' + getRandomInt(0,255) + ',' + getRandomInt(0,255) + 
