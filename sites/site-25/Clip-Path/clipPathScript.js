e70e88492a49d8f3ebcc8376c323f7fa4ebc23f3
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-07-29 15:55:58 +0300

    Upd script in site-25

--- sites/site-25/Clip-Path/clipPathScript.js
+++ sites/site-25/Clip-Path/clipPathScript.js
@@ -1,7 +1,6 @@
 
 "use strict";
 
-
 let side1 = document.getElementById('side1');
 window.addEventListener('scroll', function() {
     side1.style.left = -window.pageYOffset + 'px';
@@ -11,4 +10,3 @@ let side2 = document.getElementById('side2');
 window.addEventListener('scroll', function() {
     side2.style.left = window.pageYOffset + 'px';
 });
-
