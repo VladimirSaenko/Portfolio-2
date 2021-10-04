aee9553393d0c06474ff7fbb0644eb081a80997e
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-10-04 12:48:04 +0300

    Upd js script in site-9-2
    Upd js script in site-9-2

--- sites/site-9-2/Parrallax/mainscript.js
+++ sites/site-9-2/Parrallax/mainscript.js
@@ -8,14 +8,17 @@ function changeItem1() {
     parspace.style.width = '1050px';
     parmoon.style.width = '486px';
 }
+
 function rechangeItem1() {
     parspace.style.width = '766px';
     parmoon.style.width = '770px';
 }
+
 function changeItem2() {
     parmoon.style.width = '1050px';
     parspace.style.width = '486px';
 }
+
 function rechangeItem2() {
     parspace.style.width = '766px';
     parmoon.style.width = '770px';
