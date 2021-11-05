ba25dc71af0617157b27a1c7994f7987e6f0ce64
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2021-11-05 21:37:35 +0200

    Update js script in site-24-2
    Update js script in site-24-2

--- sites/site-24-2/Hexagon-FormX-site/cursorScript.js
+++ sites/site-24-2/Hexagon-FormX-site/cursorScript.js
@@ -2,6 +2,7 @@
 "use strict";
 
 let cursor = document.getElementById('cursor');
+
 window.onmousemove = function(e) {
     cursor.style.left = e.clientX + 'px';
     cursor.style.top = e.clientY + 'px';
