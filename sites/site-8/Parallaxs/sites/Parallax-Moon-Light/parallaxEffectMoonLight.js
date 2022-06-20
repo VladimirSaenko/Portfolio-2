c8b629107a43c010d03cc1324a880dfa07e6d995
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-06-20 18:12:39 +0300

    Update site site-8
    Update site site-8

--- sites/site-8/Parallaxs/sites/Parallax-Moon-Light/parallaxEffectMoonLight.js
+++ sites/site-8/Parallaxs/sites/Parallax-Moon-Light/parallaxEffectMoonLight.js
@@ -7,11 +7,11 @@ let mountain = document.getElementById("mountain");
 let road = document.getElementById("road");
 let text = document.getElementById("text");
 
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll',function() {
     let value = window.scrollY;
     bg.style.top = value * 0.5 + 'px';
     moon.style.left = value * 0.5 + 'px';
     mountain.style.top = value * 0.15 + 'px';
     road.style.top = value * 0.15 + 'px';
     text.style.top = value + 'px';
-})
+});
