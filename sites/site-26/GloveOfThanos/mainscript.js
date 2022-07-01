5b6ea27f2f03f7d35db086822405ee948b3e2290
switcherwow240 <56477695+VladimirSaenko@users.noreply.github.com>
2022-07-01 12:51:13 +0300

    fixed bug in site-26
    fixed bug in site-26

--- sites/site-26/GloveOfThanos/mainscript.js
+++ sites/site-26/GloveOfThanos/mainscript.js
@@ -3,16 +3,29 @@
 
 const banner = document.getElementsByClassName('banner')[0];
 const blocks = document.getElementsByClassName('blocks');
+const section = document.querySelector('section');
+let time = 14000;
 
-for(let i = 1; i < 400; i++) {
-    banner.innerHTML += "<div class='blocks'></div>";
-    const duration = Math.random() * 5;
-    blocks[i].style.animationDuration = 2 + duration + 's';
-    blocks[i].style.animationDelay = duration + 's';
+for(let i = 0; i < 10; i++) {
+    for(let j = 0; j < 10; j++) {
+        const block = document.createElement('div')
+        block.classList.add('blocks');
+        const up = 10 * j;
+        const left = 10 * i;
+        const bottom = 90 - 10 * j;
+        const right = 90 - 10 * i;
+        block.style.clipPath = `inset(${up}% ${left}% ${bottom}% ${right}%)`;
+        const duration = Math.random() * 5;
+        block.style.animationDuration = 2 + duration + 's';
+        block.style.animationDelay = duration + 's';
+        banner.append(block);
+    }
 }
 
-const section = document.querySelector('section');
-setTimeout(function() {
+setTimeout(() =>{
     section.classList.add('active');
+} , time);
+
+setTimeout(() => {
     location.reload();
-}, 13500);
+}, time + 7000)
