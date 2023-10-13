475039929cdc91c5e5d2dfcaaf0442b16380c852
witcherwow240 <vova.saenko1@gmail.com>
2023-10-14 02:06:33 +0300

    fix armor set in site iron man
    fix armor set in site iron man

--- sites/site-10/iron-man/setArmor.js
+++ sites/site-10/iron-man/setArmor.js
@@ -2,7 +2,7 @@
 
 let ironman = document.getElementById("ironman");
 
-ironman.src = `images/ironman/armors/mark${getRandomInt(1, 5)}.png`;
+ironman.src = `images/ironman/armors/mark${getRandomInt(1, 4)}.png`;
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
